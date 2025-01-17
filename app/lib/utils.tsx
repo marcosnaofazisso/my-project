27eb8867db4d32d6827b5bfe5434650899514c78
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2025-01-17 09:42:20 -0300

    add utils

---
+++ app/lib/utils.tsx
@@ -0,0 +1,22 @@
+export const isNull = (value: any | null): value is null => {
+    return value === null;
+}
+
+export const isNotNull = (value: any | null): value is any => {
+    return value !== null
+}
+
+export const isBiggerThanZero = (value: number): value is number => {
+    return value > 0
+}
+
+export const stringIsEmpty = (value: string): boolean => {
+    return value.trim().length === 0
+}
+
+export const objectIsEmpty = (value: object): boolean => {
+    return value && Object.keys(value).length === 0
+}
+export const objectIsNotEmpty = (value: object): boolean => {
+    return value && Object.keys(value).length > 0
+}
